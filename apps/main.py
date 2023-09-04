672cfa07a36aaaccdc8048ffa9e4530cab357898
AminZamani <aminzamani.work@gmail.com>
2023-09-04 09:41:26 +0330

    add, config.routers
    add a utility class for managing FastAPI routers
    also set it in main.py

--- apps/main.py
+++ apps/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from config.database import DatabaseManager
+from config.routers import RouterManager
 
 # init models
 DatabaseManager().create_database_tables()
 
 # init FastAPI
 app = FastAPI()
+
+# init routers
+RouterManager(app).import_routers()
---
+++ config/routers.py
@@ -0,0 +1,48 @@
+import importlib
+import os
+from pathlib import Path
+
+
+class RouterManager:
+    """
+    A utility class for managing FastAPI routers.
+
+    This class detects and imports FastAPI routers from 'routers.py' files in
+    the subdirectories of the 'apps' directory. It allows you to easily include
+    routers in your FastAPI application.
+
+    Attributes:
+        None
+
+    Methods:
+        import_routers():
+            Detects 'routers.py' files in subdirectories of the 'apps' directory
+            and imports the 'router' variable from each file.
+
+    Example Usage:
+        router_manager = RouterManager()
+
+        # Import routers from detected 'routers.py' files
+        router_manager.import_routers()
+    """
+
+    def __init__(self, app):
+        self.script_directory = os.path.dirname(os.path.abspath(__file__))
+        self.project_root = Path(self.script_directory).parent
+        self.app = app
+
+    def import_routers(self):
+        apps_directory = self.project_root / "apps"
+
+        for app_dir in apps_directory.iterdir():
+            if app_dir.is_dir():
+                routers_file = app_dir / "routers.py"
+                if routers_file.exists():
+                    module_name = f"apps.{app_dir.name}.routers"
+                    try:
+                        module = importlib.import_module(module_name)
+                        if hasattr(module, "router"):
+                            # Add the imported router to your FastAPI application
+                            self.app.include_router(module.router)
+                    except ImportError:
+                        pass
