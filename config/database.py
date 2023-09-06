7f4b384f0fd0277191cca96c5e6b5ad1c132ce4a
AminZamani <aminzamani.work@gmail.com>
2023-09-06 22:10:23 +0330

    Refactor, DatabaseManager class for Improved Database Management
    These improvements make it easier to work with databases in the application and ensure proper initialization and management of database resources.
    The examples provided demonstrate how to use the class for database table creation.

--- config/database.py
+++ config/database.py
@@ -1,55 +1,89 @@
 import importlib
 import os
+from operator import and_
 from pathlib import Path
-from typing import Any
-
-from sqlalchemy import create_engine, select, and_, URL
-from sqlalchemy.orm import sessionmaker, declarative_base
-from . import settings
+from sqlalchemy import create_engine, URL
+from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.orm import DeclarativeBase
-
-
-# init database
-# url = URL.create(**settings.DATABASES)
-
-
-# engine = create_engine(URL.create(**settings.DATABASES))
-
-# When working with the ORM, the session object is our main access point to the database
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+from . import settings
 
 
 class DatabaseManager:
     """
-    A utility class for managing database operations.
+    A utility class for managing database operations using SQLAlchemy.
 
-    This class simplifies the process of creating database tables by automatically
-    detecting and creating tables based on the presence of 'models.py' files in
-    the subdirectories of the 'apps' directory. It is designed to be used in
-    conjunction with SQLAlchemy and Alembic for database management.
+    The DatabaseManager simplifies the process of initializing and managing database connections, creating database
+    tables based on SQLAlchemy models, and providing a session for performing database operations.
 
     Attributes:
-        None
+        engine (Engine): The SQLAlchemy engine for the configured database.
+        session (Session): The SQLAlchemy session for database interactions.
 
     Methods:
+        __init__():
+            Initializes the DatabaseManager by creating an SQLAlchemy engine and a session based on the
+            specified database configuration from the 'settings' module.
+
         create_database_tables():
-            Detects 'models.py' files in subdirectories of the 'apps' directory
-            and creates corresponding database tables based on SQLAlchemy models.
+            Detects 'models.py' files in subdirectories of the 'apps' directory and creates corresponding
+            database tables based on SQLAlchemy models.
 
     Example Usage:
         db_manager = DatabaseManager()
 
         # Create database tables for all detected models
         db_manager.create_database_tables()
+
+    Example Usage2:
+        DatabaseManager().create_database_tables()
     """
-    engine = create_engine(URL.create(**settings.DATABASES))
-    session = None
+    engine: create_engine = None
+    session: Session = None
 
-    def __init__(self):
-        self.session = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
-        self.session = self.session()
+    @classmethod
+    def __init__(cls):
+        """
+        Initializes the DatabaseManager.
 
-    def create_database_tables(self):
+        This method creates an SQLAlchemy engine and a session based on the specified database configuration
+        from the 'settings' module.
+        For SQLite databases, it configures the engine for "multi-threaded" access.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+
+        url = URL.create(**settings.DATABASES)
+
+        # Configure the engine with or without multi-threaded support
+        if settings.DATABASES["drivername"] == "sqlite":
+
+            cls.engine = create_engine(url, connect_args={"check_same_thread": False})
+        else:
+            cls.engine = create_engine(url)
+
+        # Create an SQLAlchemy session
+        session = sessionmaker(autocommit=False, autoflush=False, bind=cls.engine)
+        cls.session = session()
+
+    @classmethod
+    def create_database_tables(cls):
+        """
+        Create database tables based on SQLAlchemy models.
+
+        This method detects 'models.py' files in subdirectories of the 'apps'
+        directory and creates corresponding database tables based on SQLAlchemy
+        models defined within those files.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         script_directory = os.path.dirname(os.path.abspath(__file__))
         project_root = Path(script_directory).parent
         apps_directory = project_root / "apps"
@@ -62,29 +96,25 @@ class DatabaseManager:
                     try:
                         module = importlib.import_module(module_name)
                         if hasattr(module, "FastModel") and hasattr(module.FastModel, "metadata"):
-                            module.FastModel.metadata.create_all(bind=self.engine)
+                            module.FastModel.metadata.create_all(bind=cls.engine)
                     except ImportError:
                         pass
 
-    @classmethod
-    def get_session(cls):
-        return cls.session
 
-
-class FastModel(DeclarativeBase, DatabaseManager):
+class FastModel(DeclarativeBase):
     """
-    A base class for creating ORM models with built-in CRUD operations.
+    A base class for creating SQLAlchemy ORM models with built-in CRUD operations.
 
-    This class provides a foundation for creating SQLAlchemy models with common
-    database operations like create and filter. It allows for convenient
-    interaction with the database while maintaining the session lifecycle.
+    This class provides a foundation for defining SQLAlchemy models with common
+    database operations like creating, updating, and querying records. It simplifies
+    database interactions while ensuring proper session management.
 
     Attributes:
         None
 
     Class Methods:
         __eq__(column, value):
-            Override the equality operator to create filter conditions.
+            Override the equality operator to create filter conditions for querying.
 
         create(**kwargs):
             Create a new instance of the model, add it to the database, and commit the transaction.
@@ -101,38 +131,69 @@ class FastModel(DeclarativeBase, DatabaseManager):
 
         # Filter products based on a condition
         active_products = Product.filter(Product.status == "active")
+
+    Args:
+        DeclarativeBase: The SQLAlchemy declarative base class from which this model inherits.
     """
 
-    # TODO develop FastModel
-    # TODO global `SessionLocal` vs class `session`
-    # TODO make SQLite as default DB, because with it we dont need to use alembic and migrations will be auto generate
-    
     @classmethod
-    def __eq__(cls, column, value):
-        # Override the equality operator to create filter conditions
-        return column == value
+    def __eq__(cls, **kwargs):
+        filter_conditions = [getattr(cls, key) == value for key, value in kwargs.items()]
+        return and_(*filter_conditions) if filter_conditions else True
+
+    # @classmethod
+    # def __eq__(cls, column, value):
+    #     """
+    #     Override the equality operator to create filter conditions for querying records.
+    #
+    #     Args:
+    #         column: The SQLAlchemy column to compare.
+    #         value: The value to compare against.
+    #
+    #     Returns:
+    #         SQLAlchemy filter condition.
+    #     """
+    #
+    #     return column == value
 
     @classmethod
     def create(cls, **kwargs):
+        """
+        Create a new instance of the model, add it to the database, and commit the transaction.
+
+        Args:
+            **kwargs: Keyword arguments representing model attributes.
+
+        Returns:
+            The newly created model instance.
+        """
+
         instance = cls(**kwargs)
-        # session = SessionLocal()
-        # session = cls.SessionLocal
+        session = DatabaseManager.session
         try:
-            cls.session.add(instance)
-            cls.session.commit()
-            cls.session.refresh(instance)
+            session.add(instance)
+            session.commit()
+            session.refresh(instance)
         except Exception as e:
-            cls.session.rollback()
+            session.rollback()
             raise
         finally:
-            cls.session.close()
+            session.close()
         return instance
 
     @classmethod
     def filter(cls, condition):
+        """
+        Retrieve records from the database based on a given filter condition.
 
-        result = None
-        with cls.session as session:
+        Args:
+            condition: SQLAlchemy filter condition.
+
+        Returns:
+            List of model instances matching the filter condition.
+        """
+
+        with DatabaseManager.session as session:
             query = session.query(cls).filter(condition)
             result = query.all()
         return result
