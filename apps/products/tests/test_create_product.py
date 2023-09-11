64e1b8d1f00da80cd525e59367b4688925342436
AminZamani <aminzamani.work@gmail.com>
2023-09-12 02:12:32 +0330

    update, product test
    set a test.db and drop tables after all tests are passed

--- apps/products/tests/test_create_product.py
+++ apps/products/tests/test_create_product.py
@@ -1,13 +1,51 @@
 from fastapi import status
 from fastapi.testclient import TestClient
+
 from apps.main import app
+from config.database import DatabaseManager
+
+"""
+class TestBlog:
+    def setup_class(self):
+        Base.metadata.create_all(engine)
+        self.session = Session()
+        self.valid_author = Author(
+            firstname="Ezzeddin",
+            lastname="Aybak",
+            email="aybak_email@gmail.com"
+        )
+
+    def teardown_class(self):
+        self.session.rollback()
+        self.session.close()
+
+    def test_author_valid(self):
+        self.session.add(self.valid_author)
+        self.session.commit()
+        aybak = self.session.query(Author).filter_by(lastname="Aybak").first()
+        assert aybak.firstname == "Ezzeddin"
+        assert aybak.lastname != "Abdullah"
+        assert aybak.email == "aybak_email@gmail.com"
+"""
 
 
 class TestProduct:
     product_endpoint = '/products/'
 
-    def setup_method(self):
-        self.client = TestClient(app)
+    @classmethod
+    def setup_class(cls):
+        cls.client = TestClient(app)
+        # Initialize the test database and session before the test class starts
+        DatabaseManager.create_test_database()
+
+    @classmethod
+    def teardown_class(cls):
+        # Drop the test database after all tests in the class have finished
+        DatabaseManager.drop_all_tables()
+
+    def setup_method(self, method):
+        # Each test method will have its own db_session fixture
+        self.db_session = DatabaseManager.session
 
     def test_invalid_with_empty_payload(self):
         """
