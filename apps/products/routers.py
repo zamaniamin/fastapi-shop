0cb8a753dc4df28f9097a1f9910ec8e433d650f5
AminZamani <aminzamani.work@gmail.com>
2023-09-18 17:13:07 +0330

    update, product routers
    - new comments
    - new router for upload media

--- apps/products/routers.py
+++ apps/products/routers.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Form, UploadFile, File
 
 from apps.products import schemas
 from apps.products.services import ProductService
@@ -8,6 +8,12 @@ router = APIRouter(
     tags=["Product"]
 )
 
+"""
+---------------------------------------
+----------- Product Routers -----------
+---------------------------------------
+"""
+
 
 @router.post(
     '/',
@@ -15,7 +21,6 @@ router = APIRouter(
     response_model=schemas.CreateProductOut
 )
 async def create_product(product: schemas.CreateProductIn):
-    # TODO add media to products
     return ProductService.create_product(product.model_dump())
 
 
@@ -25,17 +30,16 @@ async def create_product(product: schemas.CreateProductIn):
     description="Retrieve a single product."
 )
 async def retrieve_product(product_id: int):
-    # TODO GET product by ID
     return {"product": ProductService.retrieve_product(product_id)}
 
 
 @router.get(
     '/',
+    # TODO add response_model
     # response_model=schemas.ListProductOut
 )
 async def retrieve_list_produces():
     # TODO GET product list
-
     return {"products": ProductService.list_products()}
 
 
@@ -47,4 +51,28 @@ async def update_product(product_id: int, payload: schemas.UpdateProductIn):
     # TODO PUT a product (with options and variants)
     return ProductService.update_product(product_id, **payload.model_dump())
 
+
 # TODO delete a product
+
+
+"""
+---------------------------------------
+-------- Product-Media Routers --------
+---------------------------------------
+"""
+
+
+@router.post(
+    '/media',
+    status_code=status.HTTP_201_CREATED,
+    response_model=schemas.CreateProductMediaOut,
+    summary="Add new image(s)",
+    description="Add new image(s) to a product."
+)
+async def create_product_media(
+        product_id: int = Form(),
+        alt: str = Form(),
+        files: list[UploadFile] = File()
+):
+    media = ProductService.create_media(product_id=product_id, alt=alt, files=files)
+    return {'media': media}
