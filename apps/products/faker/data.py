0c42f2864b165372b7cb2e0b915c8d06d50c3279
AminZamani <aminzamani.work@gmail.com>
2023-09-20 18:23:42 +0330

    update, product fake data
    fix some bugs and refactor the code for testing usage

--- apps/products/faker/data.py
+++ apps/products/faker/data.py
@@ -1,6 +1,7 @@
 from io import BytesIO
 
 from PIL import Image
+from fastapi import UploadFile
 
 from apps.products.models import Product
 from apps.products.services import ProductService
@@ -11,29 +12,88 @@ class FakeProduct:
     Populates the database with fake products
     """
 
-    def fill_attributes(self):
+    def fill_products(self):
         ...
 
     @classmethod
-    def populate_simple_product(cls) -> tuple[dict[str, str | int], Product]:
-        """
-        Crete a full simple product (with all fields).
-        """
-        product_data = {
+    def get_payload_variable_product(cls):
+        payload = {
+            "product_name": "Test Product",
+            "description": "<p>test description</p>",
+            "status": "active",
+            "price": 25,
+            "stock": 3,
+            "options": [
+                {
+                    "option_name": "color",
+                    "items": ["red", "green"]
+                },
+                {
+                    "option_name": "material",
+                    "items": ["Cotton", "Nylon"]
+                },
+                {
+                    "option_name": "size",
+                    "items": ["M", "S"]
+                }
+            ]
+        }
+        return payload.copy()
+
+    @classmethod
+    def get_payload_simple_product(cls):
+        payload = {
             'product_name': 'test simple product',
             'description': '<p>test description</p>',
             'status': 'active',
             'price': 25,
             'stock': 3
         }
-        return product_data, ProductService.create_product(product_data, get_obj=True)
+        return payload.copy()
 
     @classmethod
-    def populate_media_files(cls):
+    def populate_simple_product(cls) -> tuple[dict[str, str | int], Product]:
         """
-        Create to media for and make them ready for upload.
+        Crete a full simple-product (with all fields).
         """
-        image1 = Image.new('RGB', (200, 100), color='red')  # Generate sample image data
+        product_data = cls.get_payload_simple_product()
+        return product_data.copy(), ProductService.create_product(product_data, get_obj=True)
+
+    @classmethod
+    def populate_variable_product(cls) -> tuple[dict[str, str | int], Product]:
+        """
+        Crete a full variable-product (with all fields).
+        """
+        product_data = cls.get_payload_variable_product()
+        return product_data.copy(), ProductService.create_product(product_data, get_obj=True)
+
+    @classmethod
+    def populate_simple_product_with_media(cls):
+        payload: dict
+        product: Product
+
+        # --- create a product ---
+        payload, product = cls.populate_simple_product()
+        payload['alt'] = 'Test Alt Text'
+
+        # --- create two image file ---
+        files = FakeMedia.populate_media_files(upload_file=True)
+
+        # --- attach media to product ---
+        media = ProductService.create_media(product.id, payload['alt'], files)
+        if media:
+            return payload, product
+
+
+class FakeMedia:
+    @classmethod
+    def populate_media_files(cls, upload_file=False):
+        """
+        Create to media and make them ready for upload.
+        """
+
+        # Generate sample image data
+        image1 = Image.new('RGB', (200, 100), color='red')
         image2 = Image.new('RGB', (100, 200), color='blue')
 
         # Convert images to bytes
@@ -43,25 +103,14 @@ class FakeProduct:
         image2.save(image2_bytes, format='JPEG')
 
         # Create a list of UploadFile objects with the image data
-        files = [
-            ('files', ('image1.jpg', image1_bytes.getvalue(), 'image/jpeg')),
-            ('files', ('image2.jpg', image2_bytes.getvalue(), 'image/jpeg')),
-        ]
+        if upload_file:
+            files = [
+                UploadFile(filename='image1.jpg', file=image1_bytes),
+                UploadFile(filename='image2.jpg', file=image2_bytes),
+            ]
+        else:
+            files = [
+                ('files', ('image1.jpg', image1_bytes.getvalue(), 'image/jpeg')),
+                ('files', ('image2.jpg', image2_bytes.getvalue(), 'image/jpeg')),
+            ]
         return files
-
-    # def populate_attributes_items(self):
-    #     """
-    #     Fil database by fake data
-    #     """
-    # for attribute_name, item_list in self.attributes.items():
-    #     attribute = AttributeQueryset.create_attribute(attribute_name)
-    #
-    #     # create a list of AttributeItem objects using a list comprehension
-    #     item_objects = [AttributeItem(attribute=attribute, item=item) for item in item_list]
-    #
-    #     # insert all the AttributeItem objects into the database in a single query
-    #     AttributeItemQueryset.bulk_create(item_objects)
-
-
-class FakeMedia:
-    ...
