b2be32c676e430628960c5fa66c312cb6e898b00
AminZamani <aminzamani.work@gmail.com>
2023-09-22 22:02:55 +0330

    update, product fake data
    add new codes for generate fake product and use in test a list of products

--- apps/products/faker/data.py
+++ apps/products/faker/data.py
@@ -1,6 +1,9 @@
+import random
 from io import BytesIO
 
 from PIL import Image
+from faker import Faker
+from faker.providers import lorem
 from fastapi import UploadFile
 
 from apps.products.models import Product
@@ -11,43 +14,79 @@ class FakeProduct:
     """
     Populates the database with fake products
     """
+    fake = Faker()
+
+    options = ['color', 'size', 'material', 'Style']
+    option_color_items = ['red', 'green', 'black', 'blue', 'yellow']
+    option_size_items = ['S', 'M', 'L', 'XL', 'XXL']
+    option_material_items = ['Cotton', 'Nylon', 'Plastic', 'Wool', 'Leather']
+    option_style_items = ['Casual', 'Formal']
 
     def fill_products(self):
-        ...
+        """
+        For generating fake products as demo
+        """
+        # TODO I should download 20 product images and save them in project and attach them for demo data
+        self.fake.add_provider(lorem)
+
+    @classmethod
+    def generate_name(cls):
+        return cls.fake.text(max_nb_chars=25)
+
+    @classmethod
+    def generate_description(cls):
+        return cls.fake.paragraph(nb_sentences=5)
+
+    @staticmethod
+    def get_random_price():
+        return round(random.uniform(1, 100), 2)
+
+    @staticmethod
+    def get_random_stock():
+        return random.randint(0, 100)
+
+    @classmethod
+    def generate_uniq_options(cls):
+        return [
+            {
+                "option_name": "color",
+                "items": cls.option_color_items[:2]
+            },
+            {
+                "option_name": "size",
+                "items": cls.option_size_items[:2]
+            },
+            {
+                "option_name": "material",
+                "items": cls.option_material_items[:2]
+            }
+        ]
+
+    """
+    new code
+    """
 
     @classmethod
     def get_payload_variable_product(cls):
+        # TODO install faker package and generate some random data for payload
         payload = {
-            "product_name": "Test Product",
-            "description": "<p>test description</p>",
-            "status": "active",
-            "price": 25,
-            "stock": 3,
-            "options": [
-                {
-                    "option_name": "color",
-                    "items": ["red", "green"]
-                },
-                {
-                    "option_name": "material",
-                    "items": ["Cotton", "Nylon"]
-                },
-                {
-                    "option_name": "size",
-                    "items": ["M", "S"]
-                }
-            ]
+            'product_name': cls.generate_name(),
+            'description': cls.generate_description(),
+            'status': 'active',
+            'price': cls.get_random_price(),
+            'stock': cls.get_random_stock(),
+            'options': cls.generate_uniq_options()
         }
         return payload.copy()
 
     @classmethod
     def get_payload_simple_product(cls):
         payload = {
-            'product_name': 'test simple product',
-            'description': '<p>test description</p>',
+            'product_name': cls.generate_name(),
+            'description': cls.generate_description(),
             'status': 'active',
-            'price': 25,
-            'stock': 3
+            'price': cls.get_random_price(),
+            'stock': cls.get_random_stock()
         }
         return payload.copy()
 
@@ -56,6 +95,7 @@ class FakeProduct:
         """
         Crete a full simple-product (with all fields).
         """
+
         product_data = cls.get_payload_simple_product()
         return product_data.copy(), ProductService.create_product(product_data, get_obj=True)
 
@@ -100,6 +140,26 @@ class FakeProduct:
         media = ProductService.create_media(product.id, payload['alt'], files)
         if media:
             return payload, product
+
+    @classmethod
+    def populate_30_product(cls):
+        # --- create 12 variable and simple products with media ---
+        # TODO generate random options for variable-products
+
+        for i in range(6):
+            cls.populate_variable_product_with_media()
+
+        for i in range(6):
+            cls.populate_simple_product_with_media()
+
+        # --- create 18 products without media ---
+        # no media
+        for i in range(9):
+            cls.populate_simple_product()
+
+        # with media
+        for i in range(9):
+            cls.populate_variable_product()
 
 
 class FakeMedia:
