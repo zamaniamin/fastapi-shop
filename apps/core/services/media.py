f363408b4f86e6a63b149c003eb4b89fbafb6afe
AminZamani <aminzamani.work@gmail.com>
2023-09-25 16:54:59 +0330

    update, Media Service
    - separate test-media directory from main dir, when running tests
    ** this section needs small refactor **

--- apps/core/services/media.py
+++ apps/core/services/media.py
@@ -1,7 +1,11 @@
+import os
 import uuid
 from pathlib import Path
 
-from fastapi import UploadFile, HTTPException
+from fastapi import UploadFile
+
+from config.database import DatabaseManager
+from config.settings import MEDIA_DIR
 
 # TODO get this from settings.py
 max_upload_size = 5
@@ -10,8 +14,13 @@ max_upload_size = 5
 # TODO set permission to access media-directory and files
 class MediaService:
     def __init__(self, parent_directory: str = "media", sub_directory: str | int = None):
-        self.path = Path(f"{parent_directory}/{sub_directory}" if sub_directory else parent_directory)
-        self.path.mkdir(parents=True, exist_ok=True)
+        testing = DatabaseManager.get_testing_mode()
+        if not testing:
+            self.path = Path(f"{MEDIA_DIR}/{parent_directory}/{sub_directory}" if sub_directory else parent_directory)
+        else:
+            self.path = Path(
+                f"{MEDIA_DIR}/test/{parent_directory}/{sub_directory}" if sub_directory else parent_directory)
+        # self.path.mkdir(parents=True, exist_ok=True)
 
     def save_file(self, file: UploadFile):
         # TODO separate exceptions to a module in core app
@@ -21,16 +30,31 @@ class MediaService:
         #     raise HTTPException(status_code=400, detail="File type not allowed")
 
         # Check if the file size is within the allowed limit in MB
-        if self.get_file_size_mb(file) > max_upload_size:
-            raise HTTPException(status_code=400, detail="File size exceeds the limit")
+        # if self.get_file_size_mb(file) > max_upload_size:
+        #     raise HTTPException(status_code=400, detail="File size exceeds the limit")
 
         # Generate a unique filename with a random string and date
         file_name = self.generate_unique_filename(file.filename)
+        if not os.path.exists(self.path):
+            os.makedirs(self.path, exist_ok=True)
 
-        with open(file_name, 'wb') as f:
+            # for file in files:
+        new_file = os.path.join(self.path, file_name)
+
+        with open(new_file, 'wb') as f:
             f.write(file.file.read())
 
+        # ======
+        # with open(file_name, 'wb') as f:
+        #     f.write(file.file.read())
+
         return file_name, file_extension
+
+    def generate_unique_filename(self, filename: str) -> str:
+        random_string = str(uuid.uuid4().hex)
+        file_extension = filename.split('.')[-1]
+        unique_filename = f"{random_string}.{file_extension}"
+        return unique_filename
 
     @staticmethod
     def is_allowed_extension(extension: str) -> bool:
@@ -41,12 +65,6 @@ class MediaService:
     def get_file_extension(file: UploadFile) -> str:
         filename_parts = file.filename.split('.')
         return filename_parts[-1] if len(filename_parts) > 1 else ""
-
-    def generate_unique_filename(self, filename: str) -> str:
-        random_string = str(uuid.uuid4().hex)
-        file_extension = filename.split('.')[-1]
-        unique_filename = f"{random_string}.{file_extension}"
-        return str(self.path / unique_filename)
 
     @staticmethod
     def get_file_size_mb(file: UploadFile) -> float:
