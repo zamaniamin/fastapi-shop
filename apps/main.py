66da8fa5385a5b4d2f3b8aeee0bcf0cde8fa1728
AminZamani <aminzamani.work@gmail.com>
2023-09-26 15:17:35 +0330

    add, StaticFiles
    to show images

--- apps/main.py
+++ apps/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
 from config.database import DatabaseManager
 from config.routers import RouterManager
+from config.settings import MEDIA_DIR
 
 # init models
 DatabaseManager().create_database_tables()
 
 # init FastAPI
 app = FastAPI()
+app.mount("/media", StaticFiles(directory=MEDIA_DIR), name="media")
 
 # init routers
 RouterManager(app).import_routers()
