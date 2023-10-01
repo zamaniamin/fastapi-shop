be269af9b4e8cf8cbc257ce6700d0c8ed009b4eb
AminZamani <aminzamani.work@gmail.com>
2023-10-01 16:05:18 +0330

    test(products), add new method

--- apps/core/base_test_case.py
+++ apps/core/base_test_case.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from apps.core.date_time import DateTime
+
 
 class BaseTestCase:
 
@@ -7,3 +9,7 @@ class BaseTestCase:
     def assert_datetime_format(date):
         formatted_date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d %H:%M:%S')
         assert date == formatted_date
+
+    @staticmethod
+    def convert_datetime_to_string(date):
+        return DateTime.string(date)
