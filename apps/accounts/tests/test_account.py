634193c5d96b4b889961436ee6675fb34d963475
AminZamani <aminzamani.work@gmail.com>
2023-10-10 02:48:20 +0330

    test(accounts), add unittest `verify_registration`

--- apps/accounts/tests/test_account.py
+++ apps/accounts/tests/test_account.py
@@ -10,6 +10,7 @@ from config.database import DatabaseManager
 
 class AccountTestBase(BaseTestCase):
     register_endpoint = "/accounts/register/"
+    register_verify_endpoint = "/accounts/register/verify/"
 
     @classmethod
     def setup_class(cls):
@@ -62,4 +63,62 @@ class TestRegister(AccountTestBase):
         assert expected_user.last_login is None
         self.assert_datetime_format(expected_user.date_joined)
 
-    # TODO test the OTP for validate the user email (after register request)
+    def test_verify_registration(self):
+        """
+        Test activating the account after verifying the OTP code (verify email).
+        """
+
+        # --- register a user ---
+        register_payload = {
+            'email': 'user@test.com',
+            'password': 'Test_12345'
+        }
+        register = AccountService.register(**register_payload)
+
+        # --- read otp code ---
+        user = UserManager.get_user(email=register_payload['email'])
+        otp_key = user.otp_key
+        otp = AccountService.read_otp(otp_key)
+
+        # --- payload ---
+        verify_payload = {
+            'email': register_payload['email'],
+            'otp': otp
+        }
+
+        # --- request ---
+        response = self.client.post(self.register_verify_endpoint, json=verify_payload)
+        assert response.status_code == status.HTTP_201_CREATED
+
+        # --- expected ---
+        expected = response.json()
+        # assert expected['token'] == payload['email']
+        assert expected['message'] == 'Your email address has been confirmed. Account activated successfully.'
+
+        expected_user = UserManager.get_user(email=register_payload['email'])
+
+        assert expected_user is not None
+        assert expected_user.id > 0
+
+        assert expected_user.email == register_payload['email']
+        assert AccountService.verify_password(register_payload['password'], expected_user.password) is True
+        assert expected_user.otp_key is None
+
+        assert expected_user.first_name is None
+        assert expected_user.last_name is None
+
+        assert expected_user.verified_email is True
+        assert expected_user.is_active is True
+        assert expected_user.is_superuser is False
+
+        assert expected_user.last_login is None
+        self.assert_datetime_format(expected_user.updated_at)
+        self.assert_datetime_format(expected_user.date_joined)
+
+    # ---------------------
+    # --- Test Payloads ---
+    # ---------------------
+
+    # TODO test `register` with invalid payloads.
+    # TODO test `verify_registration` with invalid payloads.
+    # TODO test OTP is expired
