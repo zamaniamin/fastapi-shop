f863e333aee4c65d8fd6e3034d629b1f651ed0be
AminZamani <aminzamani.work@gmail.com>
2023-10-09 22:21:35 +0330

    feat(accounts), add new endpoint `register`
    register a new user by email and password, then sent an OTP code to the email for verify its address to ensure that not a fake/wrong email.

--- apps/accounts/routers.py
+++ apps/accounts/routers.py
@@ -1,42 +1,55 @@
 from fastapi import APIRouter, status
 
-from apps.products import schemas
+from apps.accounts import schemas
+from apps.accounts.services.auth import AccountService
 
 router = APIRouter(
     prefix="/accounts",
     tags=["Authentication"]
 )
 
-# to get a string like this run:
-# Use this command to generate a key: openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
 
 @router.post(
     "/register",
     status_code=status.HTTP_201_CREATED,
-    response_model=schemas.UserSchema,
+    response_model=schemas.RegisterOut,
     summary='Register a new user',
     description='Register a new user by email,for now with email.')
-# TODO rename `user_data` to `register`
-async def register(user_data: schemas.UserRegisterSchema):
-    # TODO create a new class `OAuthService` and move the code block to it.
-    # example: registered = OAuthService.register(**register.model_dump())
-    user = UserManager.create_user(user_data.model_dump(exclude={"confirm"}))
+async def register(payload: schemas.RegisterIn):
+    return AccountService.register(**payload.model_dump(exclude={"password_confirm"}))
 
-    # TODO move this code to `OAuthService.register`
-    EmailHandler.send_totp_email(totp.generate_totp(user.totp_secret))
 
-    # TODO `return {'register': register}` , or ask from AI
-    return schemas.UserSchema.model_validate(user)
+# =====================================
+
+# to get a string like this run:
+# Use this command to generate a key: openssl rand -hex 32
+# SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+# ALGORITHM = "HS256"
+# ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+"""
+--- 2 --- verify (email / phone number) by otp code
+
+{
+  "email": "example@email.com",
+  "otp": "123456"
+}
+
+{
+  "message": "Your email address has been confirmed."
+  "JWT_Token": ""
+}
+"""
+
+# =====================================
 
 
 """
 POST /accounts/login
-POST /accounts/register
 GET /accounts/me
 PUT /accounts/me
 DELETE /accounts/me
 """
+
+# =====================================
---
+++ apps/accounts/schemas.py
@@ -0,0 +1,31 @@
+import re
+
+from pydantic import BaseModel, EmailStr, field_validator, ValidationError
+
+
+class RegisterIn(BaseModel):
+    email: EmailStr
+    password: str
+    password_confirm: str
+
+    @classmethod
+    @field_validator("password")
+    def validate_password(cls, password: str):
+        constraints = [
+            len(password) >= 8,
+            re.search(r'[0-9]', password) is not None,  # Has a number
+            re.search(r'[A-Z]', password) is not None,  # Has an Uppercase letter
+            re.search(r'[a-z]', password) is not None,  # Has a Lowercase letter
+            re.search(r'[!@#$%^&*()_+{}\[\]:;"\'<>,.?/\\|]', password) is not None  # Has a special char
+        ]
+
+        if all(constraints):
+            return password
+        raise ValidationError(
+            "Invalid password. Must contain at least 8 characters, 1 number, 1 uppercase letter, 1 lowercase "
+            "letter, and 1 special character.")
+
+
+class RegisterOut(BaseModel):
+    email: EmailStr
+    message: str
