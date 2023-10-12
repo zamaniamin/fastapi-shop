84e40284d6cd20dd77334c296086ef60f0ef7f71
AminZamani <aminzamani.work@gmail.com>
2023-10-12 16:24:08 +0330

    refactor,docs,feat(accounts)
    - add docs
    - check email verification on register
    - update last login date

--- apps/accounts/services/auth.py
+++ apps/accounts/services/auth.py
@@ -3,11 +3,16 @@ from passlib.context import CryptContext
 from pyotp import TOTP, random_base32
 
 from apps.accounts.services.user import UserManager
+from apps.core.date_time import DateTime
 from config.settings import OTP_EXPIRATION_SECONDS
 
 
 class AccountService:
     password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    # ----------------
+    # --- Register ---
+    # ----------------
 
     @classmethod
     def register(cls, **data):
@@ -41,6 +46,27 @@ class AccountService:
 
     @classmethod
     def verify_registration(cls, **data):
+        """
+        Verifies user registration by validating the provided OTP code.
+
+        For first-time users, they must validate their email address before being able to "login".
+        After validation, their account will be activated, allowing them to "login" and use the app.
+
+        During verification, the email address is validated using an OTP code. Upon successful verification,
+        the user's data is updated, the account is activated, and the user can "login" to the app.
+        Additionally, an `access_token` is sent to allow the user to "login" without being redirected to the login form.
+
+        Args:
+            data['email'] (str): User's email address.
+            data['otp'} (str): One-Time Password (OTP) code for email verification.
+
+        Raises:
+            HTTPException: If the user is not found, the email is already verified, or an invalid OTP is provided.
+
+        Returns:
+            dict: Dictionary containing an authentication token and a success message.
+        """
+
         # --- init ---
         email = data['email']
         otp = data['otp']
@@ -53,6 +79,13 @@ class AccountService:
                 detail="User not found."
             )
 
+        # --- check email verified or not ---
+        if user.verified_email:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail="This email is already verified."
+            )
+
         # --- verify otp for this user ---
         if not cls.verify_otp(user.otp_key, otp):
             raise HTTPException(
@@ -60,15 +93,36 @@ class AccountService:
                 detail="Invalid OTP code."
             )
 
-        user.update(user.id, otp_key=None, verified_email=True, is_active=True)
+        # --- Update user data and activate the account ---
+        user.update(user.id, otp_key=None, verified_email=True, is_active=True, last_login=DateTime.now())
 
-        # --- login user, generate a JWT token ---
+        # --- login user, generate and send authentication token to the client ---
+        # auth_token = cls.get_auth_token(user.id)
 
-        # set
         return {
-            'token': '',
+            'access_token': '',
             'message': 'Your email address has been confirmed. Account activated successfully.'
         }
+
+    # -------------
+    # --- Login ---
+    # -------------
+
+    # ---------------------
+    # --- Hash Password ---
+    # ---------------------
+
+    @classmethod
+    def hash_password(cls, password: str):
+        return cls.password_context.hash(password)
+
+    @classmethod
+    def verify_password(cls, plain_password: str, hashed_password: str):
+        return cls.password_context.verify(plain_password, hashed_password)
+
+    # ----------------
+    # --- OTP Code ---
+    # ----------------
 
     @classmethod
     def send_otp(cls, otp_key, email):
@@ -83,26 +137,18 @@ class AccountService:
         print(dev_show)
 
         email_body = f"""
-        Subject: Verify your email address
+            Subject: Verify your email address
 
-        Dear "{email}",
-        Thank you for registering for an account on [Your Website Name].
+            Dear "{email}",
+            Thank you for registering for an account on [Your Website Name].
 
-        To verify your email address and complete your registration, please enter the following code:
-        {otp}
+            To verify your email address and complete your registration, please enter the following code:
+            {otp}
 
-        If you do not verify your email address within 24 hours, your account will be deleted.
-        Thank you,
-        The "Fast Store" Team
-        """
-
-    @classmethod
-    def hash_password(cls, password: str):
-        return cls.password_context.hash(password)
-
-    @classmethod
-    def verify_password(cls, plain_password: str, hashed_password: str):
-        return cls.password_context.verify(plain_password, hashed_password)
+            If you do not verify your email address within 24 hours, your account will be deleted.
+            Thank you,
+            The "Fast Store" Team
+            """
 
     @staticmethod
     def generate_otp_key():
