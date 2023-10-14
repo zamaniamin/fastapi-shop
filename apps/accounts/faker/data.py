6bd89ade1e7a0e5a02c6d99863dc5237ed012b4a
AminZamani <aminzamani.work@gmail.com>
2023-10-15 00:17:04 +0330

    feat(accounts), `FakeUser` added to faker

--- apps/accounts/faker/data.py
+++ apps/accounts/faker/data.py
@@ -1,7 +1,8 @@
 from faker import Faker
 
-from apps.accounts.services.auth import AccountService
+from apps.accounts.services.auth import AccountService, AuthToken
 from apps.accounts.services.user import UserManager
+from apps.core.date_time import DateTime
 
 
 class FakeAccount:
@@ -50,6 +51,38 @@ class FakeAccount:
         otp = AccountService.read_otp(user.otp_key)
         verified = AccountService.verify_registration(**{'email': user.email, 'otp': otp})
         return user.email, verified['access_token']
+
+    @classmethod
+    def random_email(cls):
+        return cls.fake.email()
+
+
+class FakeUser:
+    fake = Faker()
+    password = 'Test_1234'
+
+    @classmethod
+    def populate_user(cls):
+        """
+        Create a new user and generate an access token too.
+        """
+
+        user_data = {
+            'email': cls.random_email(),
+            'password': cls.password,
+            'first_name': cls.fake.first_name(),
+            'last_name': cls.fake.last_name(),
+            'otp_key': None,
+            'verified_email': True,
+            'is_active': True,
+            'is_superuser': False,
+            'last_login': DateTime.now(),
+            'updated_at': DateTime.now()
+        }
+
+        user = UserManager.new_user(**user_data)
+        access_token = AuthToken.create_access_token(user.email)
+        return user, access_token
 
     @classmethod
     def random_email(cls):
