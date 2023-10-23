ee6ea4633104a4f71632268851b5bc297f51fc7c
AminZamani <aminzamani.work@gmail.com>
2023-10-23 22:49:44 +0330

    feat(accounts), add `UserChangeRequest` model
    - update docs too

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -7,6 +7,27 @@ from config.database import FastModel
 
 
 class User(FastModel):
+    """
+    User represents registered users in the application.
+
+    Attributes:
+        id (int): Unique identifier for the user.
+        email (str): User's email address used for authentication and communication.
+        password (str): Hashed password for user authentication.
+        first_name (str, optional): User's first name. Default is None.
+        last_name (str, optional): User's last name. Default is None.
+        is_verified_email (bool): Flag indicating whether the user's email address has been verified.
+        is_active (bool): Flag indicating whether the user's account is active.
+        is_superuser (bool): Flag indicating whether the user has superuser privileges.
+        role (str): User's role in the system, represented as a short string.
+        date_joined (datetime): Timestamp indicating when the user account was created.
+        updated_at (datetime, optional): Timestamp indicating when the user account was last updated. Default is None.
+        last_login (datetime, optional): Timestamp indicating the user's last login time. Default is None.
+        otp_key (ClassVar[Optional[str]]): Class variable indicating the OTP (One-Time Password) key. Default is None.
+        secret (relationship): Relationship attribute linking this user to related secrets (tokens management).
+        change (relationship): Relationship attribute linking this user to change requests initiated by the user.
+    """
+
     __tablename__ = "users"
 
     # To solve this: (Unresolved attribute reference 'otp_key' for class 'User'). when writing codes in IDE editor
@@ -31,11 +52,22 @@ class User(FastModel):
     last_login = Column(DateTime, nullable=True)
 
     secret = relationship("UserSecret", back_populates="user", cascade="all, delete-orphan")
+    change = relationship("UserChangeRequest", back_populates="user", cascade="all, delete-orphan")
 
 
 class UserSecret(FastModel):
     """
-    Temporary information for tokens management (JWT and OTP)
+    UserSecret contains temporary information for token management, including JWT and OTP tokens.
+
+    Attributes:
+        id (int): Unique identifier for the user secret entry.
+        user_id (int): ID of the user associated with this secret.
+        otp_key (str, optional): OTP (One-Time Password) key used for token-based authentication. Default is None.
+        access_token (str, optional): Access token used for JWT authentication. Default is None.
+        created_at (datetime): Timestamp indicating when this user secret entry was created.
+        updated_at (datetime, optional): Timestamp indicating when this user secret entry was last updated.
+        Default is None.
+        user (User): Relationship attribute linking this user secret entry to the associated user.
     """
 
     __tablename__ = "users_secrets"
@@ -44,9 +76,36 @@ class UserSecret(FastModel):
     user_id = Column(Integer, ForeignKey("users.id"))
 
     otp_key = Column(String, nullable=True)
-    access_token = Column(String, nullable=True)  # for more security use user IP address
+    access_token = Column(String, nullable=True)
 
     created_at = Column(DateTime, server_default=func.now())
     updated_at = Column(DateTime, nullable=True, onupdate=func.now())
 
     user = relationship("User", back_populates="secret")
+
+
+class UserChangeRequest(FastModel):
+    """
+    UserChangeRequest represents change requests initiated by users, such as email or phone number changes.
+
+    Attributes:
+        id (int): Unique identifier for the change request.
+        user_id (int): ID of the user who initiated the change request.
+        new_email (str): New email address requested by the user.
+        is_email_verified (bool): Flag indicating whether the new email address has been verified.
+        change_type (str): Indicates the type of change request ('email' or 'phone').
+        otp_key (str): OTP (One-Time Password) key used for email or phone number verification.
+        created_at (datetime): Timestamp indicating when the change request was created.
+    """
+
+    __tablename__ = "users_changes_requests"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"), unique=True)
+    new_email = Column(String(256), nullable=True)
+    # new_phone = Column(String(20), nullable=True)  # Adjust the length based on your requirements
+    change_type = Column(String(10), nullable=False)
+    otp_key = Column(String, nullable=True)
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
+
+    user = relationship("User", back_populates="change")
