57ea2df1d6b6b55d6f103eace5f3b4e276f68f09
AminZamani <aminzamani.work@gmail.com>
2023-11-25 19:52:30 +0330

    docs(accounts), add doc to `FastAPIContentType`
    Version: 0.12.0-alpha

--- apps/core/models.py
+++ apps/core/models.py
@@ -2,8 +2,11 @@ from sqlalchemy import Column, Integer, String
 
 
 class FastAPIContentType:
+    """
+    Model to handle roles and permissions on the admin panel.
+    """
     __tablename__ = "fastapi_content_type"
 
     id = Column(Integer, primary_key=True)
-    app_name = Column(String)
+    app_name = Column(String, index=True)
     model = Column(String)
