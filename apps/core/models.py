a25aac1e062bffdf044a481d91887e6600785f6f
AminZamani <aminzamani.work@gmail.com>
2023-11-26 13:10:46 +0330

    fix(accounts), update `FastAPIContentType`
    Version: 0.12.0-alpha

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,12 +1,17 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, UniqueConstraint
+
+from config.database import FastModel
 
 
-class FastAPIContentType:
+class FastAPIContentType(FastModel):
     """
     Model to handle roles and permissions on the admin panel.
     """
+
     __tablename__ = "fastapi_content_type"
 
     id = Column(Integer, primary_key=True)
     app_name = Column(String, index=True)
     model = Column(String)
+
+    __table_args__ = (UniqueConstraint(app_name, model),)
